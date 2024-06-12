675c01db0d021040326b7fbdc2754a9c08fb5086
sergiovalbuena <developergbp43@gmail.com>
2024-06-12 14:18:30 -0500

    about alts

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -8,7 +8,7 @@
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">Por Fin Jardín es un estudio de paisajismo enfocado en el jardín mediterráneo, con un toque estético muy personal y apoyado en los valores de sostenibilidad y ecología.</p>
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">Noemí Antón está al frente del Estudio junto a un equipo de profesionales con amplia formación y experiencia, que abarca arquitectos, técnicos de construcción, ilustradores gráficos, capataces y jardineros. Trabajamos principalmente en proyectos alrededor de Jávea, donde se ubica nuestra sede, Valencia, Baleares y Madrid, aunque también a nivel nacional e internacional. </p>
 					</div>
-					<img src="/img/ABOUT/01AboutStudio.jpg" alt="" class="img-slider-projects kenburns-top">
+					<img src="/img/ABOUT/01AboutStudio.jpg" alt="About - Estudio" class="img-slider-projects kenburns-top">
 				</div>
 			</swiper-slide>
 			<swiper-slide>
@@ -17,7 +17,7 @@
 						<p class="step-process Acumin-pro-wide mt-auto mb-3">Noemí Antón</p>
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">¡Hola! Soy Noemí Antón, Ingeniera Agrónoma y Paisajista con veinte años de experiencia en Proyectos de Paisajismo en clima mediterráneo y clima seco. He formado mi propio Estudio desde donde trabajo de manera totalmente personal en proyectos de diseño de jardín. Me importa mucho la comunicación con el Cliente, al que escucho atentamente, para tratar de convertir en realidad su jardín soñado de la manera más eficiente y ecológica posible.</p>
 					</div>
-					<img src="/img/ABOUT/02NoemiAnton.jpg" alt="" class="img-slider-projects kenburns-top">
+					<img src="/img/ABOUT/02NoemiAnton.jpg" alt="About - Noemí Antón" class="img-slider-projects kenburns-top">
 				</div>
 			</swiper-slide>
 			<swiper-slide>
@@ -27,7 +27,7 @@
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">Creo que el respeto al Medio Ambiente debe ser obligatorio en todas las áreas, por ello buscamos que nuestros jardines se apoyen en criterios ecológicos, usando especies mediterráneas y de clima seco, que además de requerir poca agua y mantenimiento, contribuyan a la biodiversidad.</p>
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">Nuestro objetivo es que el espacio exterior se convierta en un jardín natural, coherente y sustentado en criterios estéticos y de sostenibilidad. Un jardín que parezca que existía de antes, que no sea artificial y se llene de vida.</p>
 					</div>
-					<img src="/img/ABOUT/03Filosofia.png" alt="" class="img-slider-projects kenburns-top">
+					<img src="/img/ABOUT/03Filosofia.png" alt="About - Filosofía" class="img-slider-projects kenburns-top">
 				</div>
 			</swiper-slide>
 			<swiper-slide>
@@ -37,7 +37,7 @@
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">Mi jardín ideal es delicado, discreto, austero, sencillo y alegre. Que no transmita sensación de “decorado”, un resultado natural que se confunda con el entorno. Ordenado, pero con diferentes espacios para descubrir y disfrutar, recorridos y zonas diferenciadas. Que sea dinámico, se mueva, sorprenda. Otoñe, brote, florezca, que recuerde el paso del tiempo y ofrezca otra perspectiva vital.</p>
 						<p class="step-process-text Acumin-pro-wide Acumin-pro-wide-extra-light mb-2">Mi jardín ideal está formado por especies mediterráneas, y apenas se riega, está integrado en el espacio mediterráneo donde vivimos y subsiste por sí solo, contribuye a la biodiversidad y ofrece cobijo y alimento a los demás seres vivos que lo habitan.</p>
 					</div>
-					<img src="/img/ABOUT/04Estilo.jpg" alt="" class="img-slider-projects kenburns-top">
+					<img src="/img/ABOUT/04Estilo.jpg" alt="About - Estilo" class="img-slider-projects kenburns-top">
 				</div>
 			</swiper-slide>
 		</swiper>
