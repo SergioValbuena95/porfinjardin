76db52a4e5d50a10fa7171d794b439e8c8dd04f3
sergiovalbuena <developergbp43@gmail.com>
2024-06-12 09:51:37 -0500

    changes in movil for about and bio

--- src/pages/bio.vue
+++ src/pages/bio.vue
@@ -1,17 +1,34 @@
 <template>
-    <div class="flex relative">
-        <div class="info-bio p-8 overflow-auto">
-            <p class="step-process Acumin-pro-wide mb-3">Bio</p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Estudié en la Escuela Superior de Ingenieros Agrónomos de Madrid y mi Proyecto Fin de Carrera fue un Invernadero de Orquídeas calificado con Sobresaliente.</p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Tengo un Máster en Paisajismo por la Escuela Técnica Superior de Ingenieros Agrómomos y la Escuela Técnica Superior de Arquitectura de Madrid y una experiencia de más de 20 años en paisajismo en España y fuera.</p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Mientras acababa mi carrera trabajé como becaria en el Departamento de Producción Vegetal de la Escuela de Agrónomos durante cuatro años. Allí aprendí muchas cosas, la principal de ellas el significado de la palabra compañerismo. También a escribir con rigurosidad científica, a conocer el trabajo de campo y pasar por todo el espectro agrario: desde ayudante de campo hasta responsable como Ingeniera Superior en Proyectos Europeos, en temas relacionados con semillas, cultivos, heladas y fertilización. Fue una etapa preciosa que recuerdo con mucho cariño.</p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Terminando de cursar el Máster me fichó el Estudio de Paisajismo Dehesa de la Jara, os sonarán pues son los que editan la Base de Precios de Paisajismo (si no la conoces pincha aquí: enlace https://basepaisajismo.com/). Allí aprendí de verdad a preparar un Proyecto de Paisajismo: formal, riguroso, exhaustivo, absolutamente definido. Hacíamos obra pública y el equipo era increíble, así como la intensidad del trabajo. Fue una etapa muy intensa y muy feliz. </p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Después entré a formar parte del equipo del paisajista Luis Vallejo, una absoluta referencia en el paisajismo en España, con quien permanecísiete años. Llevábamos a cabo proyectos de gran envergadura, muy especiales, entre los que destaca el Jardín Botánico de Omán, la Ciudad Financiera del Banco Santander, diversos Hospitales Públicos y Proyectos exclusivos de viviendas de particulares. Fue una etapa muy bonita donde tuve la suerte de formar parte de proyectos increíbles al lado de un Maestro.</p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Actualmente vivo en Jávea, un pueblo marinero a orillas del Mediterráneo y he formado el Estudio Por Fin Jardín, desde donde proyectamos y desarrollamos proyectos de paisajismo, principalmente en Jávea, Ibiza, Madrid, Valencia, y también en cualquier provincia de España, hemos trabajado en Ávila, Salamanca, Hamburgo o Ginebra.</p>
-            <p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Puedes utilizar mi Estudio, mi trayectoria, y mi experiencia para ayudarte a conseguir tu jardín soñado. </p>
-        </div>
-        <img src="/img/ABOUT/noemiAnton.jpg" alt="" class="img-slider-projects kenburns-top">
-    </div>
+	<div class="hidden lg:block">
+		<div class="flex relative">
+			<div class="info-bio p-8 overflow-auto">
+				<p class="step-process Acumin-pro-wide mb-3">Bio</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Estudié en la Escuela Superior de Ingenieros Agrónomos de Madrid y mi Proyecto Fin de Carrera fue un Invernadero de Orquídeas calificado con Sobresaliente.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Tengo un Máster en Paisajismo por la Escuela Técnica Superior de Ingenieros Agrómomos y la Escuela Técnica Superior de Arquitectura de Madrid y una experiencia de más de 20 años en paisajismo en España y fuera.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Mientras acababa mi carrera trabajé como becaria en el Departamento de Producción Vegetal de la Escuela de Agrónomos durante cuatro años. Allí aprendí muchas cosas, la principal de ellas el significado de la palabra compañerismo. También a escribir con rigurosidad científica, a conocer el trabajo de campo y pasar por todo el espectro agrario: desde ayudante de campo hasta responsable como Ingeniera Superior en Proyectos Europeos, en temas relacionados con semillas, cultivos, heladas y fertilización. Fue una etapa preciosa que recuerdo con mucho cariño.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Terminando de cursar el Máster me fichó el Estudio de Paisajismo Dehesa de la Jara, os sonarán pues son los que editan la Base de Precios de Paisajismo (si no la conoces pincha aquí: enlace https://basepaisajismo.com/). Allí aprendí de verdad a preparar un Proyecto de Paisajismo: formal, riguroso, exhaustivo, absolutamente definido. Hacíamos obra pública y el equipo era increíble, así como la intensidad del trabajo. Fue una etapa muy intensa y muy feliz. </p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Después entré a formar parte del equipo del paisajista Luis Vallejo, una absoluta referencia en el paisajismo en España, con quien permanecísiete años. Llevábamos a cabo proyectos de gran envergadura, muy especiales, entre los que destaca el Jardín Botánico de Omán, la Ciudad Financiera del Banco Santander, diversos Hospitales Públicos y Proyectos exclusivos de viviendas de particulares. Fue una etapa muy bonita donde tuve la suerte de formar parte de proyectos increíbles al lado de un Maestro.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Actualmente vivo en Jávea, un pueblo marinero a orillas del Mediterráneo y he formado el Estudio Por Fin Jardín, desde donde proyectamos y desarrollamos proyectos de paisajismo, principalmente en Jávea, Ibiza, Madrid, Valencia, y también en cualquier provincia de España, hemos trabajado en Ávila, Salamanca, Hamburgo o Ginebra.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Puedes utilizar mi Estudio, mi trayectoria, y mi experiencia para ayudarte a conseguir tu jardín soñado. </p>
+			</div>
+			<img src="/img/ABOUT/noemiAnton.jpg" alt="" class="img-bio kenburns-top">
+		</div>
+	</div>
+	<div class="lg:hidden">
+		<div class="flex flex-col relative">
+			<img src="/img/ABOUT/noemiAnton.jpg" alt="" class="">
+			<div class="flex flex-col p-8 overflow-auto">
+				<p class="step-process Acumin-pro-wide mb-3">Bio</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Estudié en la Escuela Superior de Ingenieros Agrónomos de Madrid y mi Proyecto Fin de Carrera fue un Invernadero de Orquídeas calificado con Sobresaliente.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Tengo un Máster en Paisajismo por la Escuela Técnica Superior de Ingenieros Agrómomos y la Escuela Técnica Superior de Arquitectura de Madrid y una experiencia de más de 20 años en paisajismo en España y fuera.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Mientras acababa mi carrera trabajé como becaria en el Departamento de Producción Vegetal de la Escuela de Agrónomos durante cuatro años. Allí aprendí muchas cosas, la principal de ellas el significado de la palabra compañerismo. También a escribir con rigurosidad científica, a conocer el trabajo de campo y pasar por todo el espectro agrario: desde ayudante de campo hasta responsable como Ingeniera Superior en Proyectos Europeos, en temas relacionados con semillas, cultivos, heladas y fertilización. Fue una etapa preciosa que recuerdo con mucho cariño.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Terminando de cursar el Máster me fichó el Estudio de Paisajismo Dehesa de la Jara, os sonarán pues son los que editan la Base de Precios de Paisajismo (si no la conoces pincha aquí: enlace https://basepaisajismo.com/). Allí aprendí de verdad a preparar un Proyecto de Paisajismo: formal, riguroso, exhaustivo, absolutamente definido. Hacíamos obra pública y el equipo era increíble, así como la intensidad del trabajo. Fue una etapa muy intensa y muy feliz. </p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Después entré a formar parte del equipo del paisajista Luis Vallejo, una absoluta referencia en el paisajismo en España, con quien permanecísiete años. Llevábamos a cabo proyectos de gran envergadura, muy especiales, entre los que destaca el Jardín Botánico de Omán, la Ciudad Financiera del Banco Santander, diversos Hospitales Públicos y Proyectos exclusivos de viviendas de particulares. Fue una etapa muy bonita donde tuve la suerte de formar parte de proyectos increíbles al lado de un Maestro.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Actualmente vivo en Jávea, un pueblo marinero a orillas del Mediterráneo y he formado el Estudio Por Fin Jardín, desde donde proyectamos y desarrollamos proyectos de paisajismo, principalmente en Jávea, Ibiza, Madrid, Valencia, y también en cualquier provincia de España, hemos trabajado en Ávila, Salamanca, Hamburgo o Ginebra.</p>
+				<p class="step-process-text Acumin-pro-wide Acumin-pro-wide mb-3">Puedes utilizar mi Estudio, mi trayectoria, y mi experiencia para ayudarte a conseguir tu jardín soñado. </p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -23,7 +40,7 @@
 ::-webkit-scrollbar { background-color: transparent; width: 10px; height: 6px; }
 ::-webkit-scrollbar-thumb { -webkit-box-shadow:inset 0 0 6px rgba(0,0,0,0.1); background-color:#bfbfbfcf; }
 
-.img-slider-projects {
+.img-bio {
     height: calc(100dvh - 84px);
     object-fit: cover;
     width: 80%;
