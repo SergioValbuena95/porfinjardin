adfa3b2b5ad17cb51ea84c3b960fbbe7a57fca59
SergioValbuenaBolt <sergio.jimenez@boltontechnology.com>
2024-07-11 09:42:45 -0500

    +- projects details  adjust w in img

--- src/pages/proyectosVerDynamicV2.vue
+++ src/pages/proyectosVerDynamicV2.vue
@@ -23,7 +23,7 @@
 						navigation
 						class="h-full">
 							<swiper-slide v-for="(img, idx) in item.imagenes" :key="idx">
-								<img :src="img" :alt="`Render ${idx+1} de ${item.titulo}`" class="w-100 h-full">
+								<img :src="img" :alt="`Render ${idx+1} de ${item.titulo}`" class="w-full h-full">
 							</swiper-slide>
 						</swiper>
 					</div>
